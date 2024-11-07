824d9eba16a5e8c74c7d9325b91ac08a65a25113
Ciaran Harris <harrisciaran1@gmail.com>
2024-11-07 14:09:52 +0000

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,6 +1,26 @@
 public class Employee {
-  private String name;
-  private int employeeId;
-  private int salaryScale;
-  private String position;
+    private int employeeId;
+    private String name;
+    private double salary;
+    private int salaryScale;
+    private String position;
+
+    public Employee(int employeeId, String name,String position, int salaryScale) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.position = position;
+        this.salary = getSalary(position, salaryScale);
+    }
+
+    public double getSalary(String position, int salaryScale) {
+        return SalaryScale.getSalaryScaleForPoint(position, salaryScale);
+    }
+
+        public String toString() {
+            return "Employee ID: " + employeeId +
+                    "\nName: " + name +
+                    "\nPosition: " + position +
+                    "\nSalary: " + salary;
+        }
+    }
 }

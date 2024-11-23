862052c9d988c94bc9db1f7913837b51260154e9
Ciaran Harris <harrisciaran1@gmail.com>
2024-11-23 00:40:09 +0000

    Add files via upload

--- PayrollSystem.java
+++ PayrollSystem.java
@@ -5,73 +5,101 @@ import java.util.Scanner;
 
 public class PayrollSystem {
     private ArrayList<Employee> employees;
-    private final String password = "0000";
+    private final String adminPassword = "0000";
+    private final String hrPassword = "1111";
 
     public PayrollSystem() {
-        employees = new ArrayList<Employee>();
+        employees = new ArrayList<>();
     }
 
-    public ArrayList<Employee> getEmployees() {
+    public ArrayList<Employee> getEmployees(){
         return employees;
     }
 
-
     public Employee getEmployee(String employeeId) {
         for (Employee employee : employees) {
-            if (employee.getEmployeeId().equals(employeeId)){
+            if (employee.getEmployeeId().equals(employeeId)) {
                 return employee;
             }
         }
         return null;
     }
 
-    public boolean employeeCheck(String employeeId){
-        boolean result = false;
-        for (Employee e : employees) {
-            String id = e.getEmployeeId();
-            if (id.equals(employeeId)) {
-                result = true;
-                break;
+
+        public boolean employeeCheck (String employeeId){
+            boolean result = false;
+            for (Employee e : employees) {
+                String id = e.getEmployeeId();
+                if (id.equals(employeeId)) {
+                    result = true;
+                    break;
+                }
+            }
+            if (!result) {
+                System.err.println("Warning: Employee ID not found.");
+            }
+            return result;
+        }
+
+        public boolean adminCheck(String employeeId) {
+            String position;
+            boolean result = false;
+
+            if (employeeCheck(employeeId)) {
+                position = getEmployee(employeeId).getPosition();
+
+                if (position.contains("ADMIN")) {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public void addEmployee (Employee employee){
+            employees.add(employee);
+        }
+
+
+        //Wait until HR and Salary Scale class is done
+        public void promoteEmployee (String employeeId, String salaryScale){
+            //HR.promoteEmployee(employeeId, salaryScale);
+        }
+
+        public boolean adminPasswordCheck (String otherPassword){
+            if (otherPassword.equals(adminPassword)) {
+                return true;
+            } else {
+                return false;
             }
         }
-        if (!result) {
-            System.err.println("Warning: Employee ID not found.");
+
+        public boolean hrPasswordCheck(String otherPassword) {
+            if (otherPassword.equals(hrPassword)) {
+                return true;
+            } else {
+                return false;
+            }
         }
-        return result;
-    }
 
-    public void addEmployee(Employee employee){
-        employees.add(employee);
-    }
+        public String getEmployeeDetails (String employeeId){
+            return getEmployee(employeeId).toString();
+        }
 
-    //Wait until HR and Salary Scale class is done
-    public void promoteEmployee (String employeeId, String salaryScale){
-        //HR.promoteEmployee(employeeId, salaryScale);
-    }
-
-    public void populatePayslips(){
+        public void populatePaySlips() {
         CSVReaderPayslip reader = new CSVReaderPayslip();
         reader.readPayslipsFromCSV(this);
-    }
+        }
 
-    public void writePayslips(){
-        CSVWriterPayslip writer = new CSVWriterPayslip();
-        writer.writePayslipsToCSV(this);
-    }
+        public void writePayslips() {
+            CSVWriterPayslip writer = new CSVWriterPayslip();
+            writer.writePayslipsToCSV(this);
+        }
 
-    public boolean passwordCheck(String otherPassword){
-        return otherPassword.equals(password);
-    }
-
-    public StringBuilder getEmployeeIds(){
+        public StringBuilder getEmployeeIds() {
         StringBuilder s = new StringBuilder();
-        for (Employee e : employees){
+        for (Employee e : employees) {
             s.append(e.getEmployeeId()).append("\n");
         }
         return s;
+        }
     }
-
-    public String getEmployeeDetails(String employeeId){
-        return getEmployee(employeeId).toString();
-    }
-}
--- PayrollSystemMenu.java
+++ PayrollSystemMenu.java
@@ -5,6 +5,9 @@ public class PayrollSystemMenu {
 
     private Scanner input;
     private CSVReaderEmployee employeeReader = new CSVReaderEmployee();
+    private String employeeId;
+    private String command;
+    private String password;
 
     public PayrollSystemMenu() {
         input = new Scanner(System.in);
@@ -20,78 +23,26 @@ public class PayrollSystemMenu {
         while (more) {
 
             System.out.println("Input user type: E)mployee   H)uman Resources   A)dmin   Q)uit");
-            String command = input.nextLine().toUpperCase();
+            command = input.nextLine().toUpperCase();
 
             if (command.equals("Q")) {
                 more = false;
             }
 
             if (command.equals("A")) {
+                System.out.println("Input employee Id: ");
+                employeeId = input.nextLine().toUpperCase();
 
-                boolean adminMenu = true;
-
-                while (adminMenu) {
+                if (payroll.adminCheck(employeeId)) {
                     System.out.println("Input password: ");
-                    String password = input.nextLine().toUpperCase();
+                    password = input.nextLine().toUpperCase();
 
-                    if (payroll.passwordCheck(password) == true) {
-                        System.out.println("S)how most recent payslip   E)mployee Details   P)ayslip History   A)dd Employee   R)eturn all Employees   Q)uit");
-                        command = input.nextLine().toUpperCase();
+                    if (payroll.adminPasswordCheck(password)) {
+                        boolean adminMenu = true;
 
-                        if (command.equals("S")) {
-                            //System.out.println(payroll.getPaySlip(employeeId));
-                        } else if (command.equals("E")) {
-                            System.out.println(payroll.toString());
-                        } else if (command.equals("P")) {
-                            //System.out.println(payroll.getPayslipHistory(employeeId));
-                        } else if (command.equals("A")) {
-                            System.out.println("Input details of employee you would like to add");
-
-                            System.out.println("Enter Employee Id: ");
-                            String newEmployeeId = input.nextLine();
-
-                            System.out.println("Enter Employee Name: ");
-                            String newEmployeeName = input.nextLine();
-
-                            System.out.println("Enter Employee Position: ");
-                            String newEmployeePosition = input.nextLine();
-
-                            System.out.println("Enter Employee Salary Scale: ");
-                            int newSalaryScale = Integer.parseInt(input.nextLine());
-
-                            System.out.println("Enter Date Of Employment: ");
-                            String newDateOfEmployment = input.nextLine();
-
-                            Employee newEmployee = new Employee(newEmployeeId, newEmployeeName, newEmployeePosition, newSalaryScale, newDateOfEmployment);
-
-                            payroll.addEmployee(newEmployee);
-
-
-                        } else if (command.equals("R")) {
-                            System.out.println(payroll.getEmployees());
-
-                        } else if (command.equals("Q")) {
-                            adminMenu = false;
-                        }
-                    }else {
-                        adminMenu = false;
-                    }
-                }
-            }
-
-            if (command.equals("E") || command.equals("H")) {
-                System.out.println("Input Employee Id");
-                String employeeId = input.nextLine().toUpperCase();
-
-
-                if (payroll.employeeCheck(employeeId)) {
-                    if (command.equals("E")) {
-
-                        boolean employeeMenu = true;
-
-                        while (employeeMenu) {
-                            System.out.println("S)how most recent payslip   E)mployee Details   P)ayslip History   Q)uit");
-                            command = input.nextLine().toUpperCase();
+                        while (adminMenu) {
+                            System.out.println("S)how most recent payslip   E)mployee Details   P)ayslip History   A)dd Employee   R)eturn all Employees   Q)uit");
+                            command= input.nextLine().toUpperCase();
 
                             if (command.equals("S")) {
                                 System.out.println("\n" + payroll.getEmployee(employeeId).getFirstPayslip().toString());
@@ -99,45 +50,129 @@ public class PayrollSystemMenu {
                                 System.out.println(payroll.getEmployeeDetails(employeeId));
                             } else if (command.equals("P")) {
                                 payroll.getEmployee(employeeId).printAllPayslips();
+                            } else if (command.equals("A")) {
+                                System.out.println("Input details of employee you would like to add");
+
+                                System.out.println("Enter Employee Id: ");
+                                String newEmployeeId = input.nextLine();
+
+                                System.out.println("Enter Employee Name: ");
+                                String newEmployeeName = input.nextLine();
+
+                                System.out.println("Enter Employee Position: ");
+                                String newEmployeePosition = input.nextLine();
+
+                                System.out.println("Enter Employee Salary Scale: ");
+                                int newSalaryScale = Integer.parseInt(input.nextLine());
+
+                                System.out.println("Enter Date Of Employment: ");
+                                String newDateOfEmployment = input.nextLine();
+
+                                Employee newEmployee = new Employee(newEmployeeId, newEmployeeName, newEmployeePosition, newSalaryScale, newDateOfEmployment);
+
+                                payroll.addEmployee(newEmployee);
+
+
+                            } else if (command.equals("R")) {
+                                System.out.println(payroll.getEmployees());
+
                             } else if (command.equals("Q")) {
-                                employeeMenu = false;
+                                adminMenu = false;
                             }
                         }
-                    } else if (command.equals("H")) {
+                    }
+                }
+            }
 
-                        boolean hrMenu = true;
+            if (command.equals("E") || command.equals("H")) {
+                System.out.println("Input Employee Id");
+                employeeId = input.nextLine().toUpperCase();
 
-                        while (hrMenu) {
 
-                            System.out.println("S)how most recent payslip   E)mployee Details   P)ayslip History   PR)omote options   Q)uit");
-                            command = input.nextLine().toUpperCase();
+                if (payroll.employeeCheck(employeeId)) {
+                    if (command.equals("E")) {
+                        //if (payroll.getContractType(employeeId) = "FULL-TIME") {
+                            boolean employeeMenu = true;
 
-                            if (command.equals("S")) {
-                                //System.out.println(payroll.getPaySlip(employeeId));
-                            } else if (command.equals("E")) {
-                                System.out.println(payroll.toString());
-                            } else if (command.equals("P")) {
-                                //System.out.println(payroll.getPayslipHistory(employeeId));
-                            } else if (command.equals("Q")) {
-                                more = false;
-                            } else if (command.equals("PR")) {
-                                System.out.println("Input Employee Id that you would like to promote");
-                                String promotionId = input.nextLine().toUpperCase();
-
-                                System.out.println(payroll.toString());
-
-                                // Print out how long the employee has been a part of the system also
-                                System.out.println("Input next Salary Scale you would like the employee to be promoted to");
-                                String salaryScale = input.nextLine().toUpperCase();
-
-                                System.out.println("Would you like to confirm this promotion: Y)es or N)o");
+                            while (employeeMenu) {
+                                System.out.println("S)how most recent payslip   E)mployee Details   P)ayslip History   Q)uit");
                                 command = input.nextLine().toUpperCase();
 
-                                if (command.equals("Y")) {
-                                    payroll.promoteEmployee(employeeId, salaryScale);
-                                    hrMenu = false;
-                                } else if (command.equals("N")) {
-                                    hrMenu = false;
+                                if (command.equals("S")) {
+                                    System.out.println("\n" + payroll.getEmployee(employeeId).getFirstPayslip().toString());
+                                } else if (command.equals("E")) {
+                                    System.out.println(payroll.getEmployeeDetails(employeeId));
+                                } else if (command.equals("P")) {
+                                    payroll.getEmployee(employeeId).printAllPayslips();
+                                } else if (command.equals("Q")) {
+                                    employeeMenu = false;
+                                }
+                            }
+                        } //else if (payroll.getContractType(employeeId) = "PART-TIME") {
+                            boolean employeeMenu = true;
+
+                            while (employeeMenu) {
+                                System.out.println("S)how most recent payslip   E)mployee Details   P)ayslip History   S)ubmit Claim   Q)uit");
+                                command = input.nextLine().toUpperCase();
+
+                                if (command.equals("S")) {
+                                    System.out.println("\n" + payroll.getEmployee(employeeId).getFirstPayslip().toString());
+                                } else if (command.equals("E")) {
+                                    System.out.println(payroll.getEmployeeDetails(employeeId));
+                                } else if (command.equals("P")) {
+                                    payroll.getEmployee(employeeId).printAllPayslips();
+                                } else if (command.equals("S")) {
+                                    //Check date on system. If its second friday of month or gone past then this function isnt possible
+
+                                    System.out.println("Input hours worked this month: ");
+                                    int hoursWorked = Integer.parseInt(input.nextLine());
+
+                                    //payroll.submitPayClaim(hoursWorked);
+                                } else if (command.equals("Q")) {
+                                    employeeMenu = false;
+                                }
+                            }
+
+                        }
+                    }else if (command.equals("H")) {
+
+                        System.out.println("Input password: ");
+                        password = input.nextLine();
+
+                        if (payroll.hrPasswordCheck(password)) {
+                            boolean hrMenu = true;
+
+                            while (hrMenu) {
+
+                                System.out.println("S)how most recent payslip   E)mployee Details   P)ayslip History   PR)omote options   Q)uit");
+                                command = input.nextLine().toUpperCase();
+
+                                if (command.equals("S")) {
+                                    System.out.println("\n" + payroll.getEmployee(employeeId).getFirstPayslip().toString());
+                                } else if (command.equals("E")) {
+                                    System.out.println(payroll.getEmployeeDetails(employeeId));
+                                } else if (command.equals("P")) {
+                                    payroll.getEmployee(employeeId).printAllPayslips();
+                                } else if (command.equals("Q")) {
+                                    more = false;
+                                } else if (command.equals("PR")) {
+                                    System.out.println("Input Employee Id that you would like to promote");
+                                    String promotionId = input.nextLine().toUpperCase();
+
+
+                                    // Print out how long the employee has been a part of the system also
+                                    System.out.println("Input next Salary Scale you would like the employee to be promoted to");
+                                    String salaryScale = input.nextLine().toUpperCase();
+
+                                    System.out.println("Would you like to confirm this promotion: Y)es or N)o");
+                                    command = input.nextLine().toUpperCase();
+
+                                    if (command.equals("Y")) {
+                                        payroll.promoteEmployee(employeeId, salaryScale);
+                                        System.out.println("Promotion Offer has been sent to: " + payroll.getEmployee(promotionId).getName());
+                                    } else if (command.equals("N")) {
+                                        System.out.println("Promotion Offer has been cancelled.");
+                                    }
                                 }
                             }
                         }
@@ -145,5 +180,4 @@ public class PayrollSystemMenu {
                 }
             }
         }
-    }
-}
+
